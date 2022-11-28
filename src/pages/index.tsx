ca8c1df4e82d980e2ffebc69dde4b906f7da6d31
Michael C Grant <Michael.Collier.Grant@gmail.com>
2022-11-28 13:34:49 -0600

    adapting tailwind ui navbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,13 +14,13 @@ const Home: NextPage = () => {
       <Head>
         <title>Vote on Articles</title>
         <meta name="description" content="Give your opinion on the top stories of the day" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/icons/favicon.ico" />
       </Head>
       <Navbar />
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
+          Create  <span className="text-[hsl(280,100%,70%)]">T3</span> App
           </h1>
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
             <Link
@@ -28,7 +28,7 @@ const Home: NextPage = () => {
               href="https://create.t3.gg/en/usage/first-steps"
             >
               <h3 className="text-2xl font-bold">First Steps →</h3>
-              <div className="text-lg">
+              <div className="text-lg font-prose">
                 Just the basics - Everything you need to know to set up your
                 database and authentication.
               </div>
@@ -38,14 +38,14 @@ const Home: NextPage = () => {
               href="https://create.t3.gg/en/introduction"
             >
               <h3 className="text-2xl font-bold">Documentation →</h3>
-              <div className="text-lg">
+              <div className="text-lg font-prose">
                 Learn more about Create T3 App, the libraries it uses, and how
                 to deploy it.
               </div>
             </Link>
           </div>
           <div className="flex flex-col items-center gap-2">
-            <p className="text-2xl text-white">
+            <p className="text-2xl text-white font-prose">
               {hello.data ? hello.data.greeting : "Loading tRPC query..."}
             </p>
             <AuthShowcase />
