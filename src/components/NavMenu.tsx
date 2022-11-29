40f9f2f6f1f42c4352db1e14eb8effd2227c185b
MichaelCGrant <MichaelCGrant@gmail.com>
2022-11-29 05:43:06 +0000

    removed unnecessary component

--- src/components/NavMenu.tsx
+++
@@ -1,9 +0,0 @@
-const NavMenu:React.FC = () => {
-  return (
-    <nav className="grow flex justify-start">
-      <h1 className="p-5">This is the Navbar!</h1>
-    </nav>
-  );
-}
-
-export default NavMenu;
